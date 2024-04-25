cf6b7a05367ee0ab9b2800790a6ffdb648a12f71
ahmed-my <yahaya.mymohammed@gmail.com>
2024-04-25 00:05:38 +0000

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-var */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const data = true;
-      resolve(data);
-    }, 1000);
-  });
+  return new Promise(() => {});
 }
