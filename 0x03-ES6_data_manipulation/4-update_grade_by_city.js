006d0c0653bf491f0da51365149645a31a7d8ac4
ahmed-my <yahaya.mymohammed@gmail.com>
2024-04-29 14:21:31 +0000

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,24 +1,23 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   // Filter students by city
-  const filteredStudents = students.filter(student => student.location === city);
+  const filteredStudents = students.filter((student) => student.location === city);
 
   // Map over filtered students and update their grades
-  const updatedStudents = filteredStudents.map(student => {
+  const updatedStudents = filteredStudents.map((student) => {
     // Find the grade for the current student from newGrades array
-    const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-    
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+
     // If grade exists, assign it to the student, otherwise set grade to 'N/A'
     const grade = gradeObj ? gradeObj.grade : 'N/A';
-    
+
     // Return updated student object
     return {
       id: student.id,
       firstName: student.firstName,
       location: student.location,
-      grade: grade
+      grade,
     };
   });
 
   return updatedStudents;
 }
-
