bacb59c7f87bf16ba3f00315395097813cf13251
ahmed-my <yahaya.mymohammed@gmail.com>
2024-04-29 21:36:22 +0000

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,16 @@
 export default function cleanSet(set, startString) {
-  // Check if startString is empty
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString === '') {
+  // Check if any of the conditions for returning empty string are met
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString === '' || set.size === 0) {
     return '';
   }
+
   // Filter set values that start with the specified string
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
+
+  // Check if filtered values array is empty
+  if (filteredValues.length === 0) {
+    return '';
+  }
 
   // Remove the startString from the filtered values
   const cleanedValues = filteredValues.map((value) => value.replace(startString, ''));
