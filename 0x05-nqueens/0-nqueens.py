b2ef09909764ab863284346202c10d23dbbc9159
my-ahmed <yahaya.mymohammed@gmail.com>
2024-05-30 22:06:44 +0000

    alx interview Nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -15,25 +15,24 @@ if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
 
-number = int(sys.argv[1])
+n = int(sys.argv[1])
 
 
-def queens(number, i=0, a=[], b=[], c=[]):
+def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
-    if i < number:
-        for j in range(number):
+    if i < n:
+        for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
-                yield from
-                queens(number, i + 1, a + [j], b + [i + j], c + [i - j])
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
 
 
-def solve(number):
+def solve(n):
     """ solve """
     k = []
     i = 0
-    for solution in queens(number, 0):
+    for solution in queens(n, 0):
         for s in solution:
             k.append([i, s])
             i += 1
@@ -42,4 +41,4 @@ def solve(number):
         i = 0
 
 
-solve(number)
+solve(n)
