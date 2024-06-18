d39c6db589c334b1dc9167510f59ab855a24523b
my-ahmed <yahaya.mymohammed@gmail.com>
2024-06-18 22:39:02 +0000

    alx interview making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,11 @@ Program to determine the fewest number of coins
 
 
 def makeChange(coins, total):
-    """
-    function to determine the fewest number of coins
-    needed to meet a given amount total
-    """
     if total <= 0:
         return 0
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
+
     for coin in coins:
         for number in range(coin, total + 1):
             dp[number] = min(dp[number], dp[number - coin] + 1)
